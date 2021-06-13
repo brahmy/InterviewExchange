4b594871027bcd81159c7f3dd8484d74329c2760
brahmaiah <Brahmaiah@143>
2021-06-13 16:55:55 +0530

    New Changes

--- app/src/main/java/com/asab/interviewexchange/MainActivity.kt
+++ app/src/main/java/com/asab/interviewexchange/MainActivity.kt
@@ -63,7 +63,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_share -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
